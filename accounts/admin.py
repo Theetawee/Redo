d894dbc56c6ecdbec7ed4d620378bee6c65308c2
Forever Inc dev <redodevs@gmail.com>
2023-08-05 13:22:13 +0300

    save

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    # Fields to be displayed in the list view of the admin interface
+    list_display = ('id', 'username', 'email', 'is_active', 'date_joined')
+    
+    # Fields that can be used for filtering the accounts in the admin interface
+    list_filter = ('is_active', 'date_joined')
+    
+    # Fields that can be searched in the admin interface
+    search_fields = ('username', 'email')
+
+    # Customize the display of the individual account's detail page
+    # You can use 'fields' or 'fieldsets' to customize the layout.
+    # Example using 'fields':
+    # fields = ('username', 'email', 'is_active', 'date_joined')
+    
+    # Example using 'fieldsets':
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('username', 'email')
+        }),
+        ('Status', {
+            'fields': ('is_active', 'date_joined','status')
+        }),
+    )
+
+# Register the Account model with the custom admin class
+admin.site.register(Account, AccountAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class Account(AbstractUser):
+    STATUS = [
+        ('verified', 'Verified'),
+        ('regular', 'Regular')
+    ]
+    status = models.CharField(choices=STATUS, default='regular', max_length=9)
+    email = models.EmailField(unique=True)
+
+    # Set the email field as the USERNAME_FIELD for authentication
+    USERNAME_FIELD = 'email'
+    # Add 'email' to the REQUIRED_FIELDS for the createsuperuser command
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
