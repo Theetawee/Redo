ad62e2cd7ead288212d53926896bd99e5278ab71
Forever Inc dev <redodevs@gmail.com>
2023-08-06 17:24:38 +0300

    save

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import homepage
+from django.urls import path,re_path
+from .views import homepage,service_worker,manifest
 
 
 urlpatterns=[
-    path('',homepage,name='home')
+    path('',homepage,name='home'),
+    re_path(r'^serviceworker\.js$', service_worker, name='serviceworker'),
+    re_path(r'^manifest\.json$', manifest, name='manifest'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from accounts.decorators import verified_email_required
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
+import os
+from pathlib import Path
 
-# Create your views here.
+
+
 @login_required
 @verified_email_required
 def homepage(request):
     return render(request,'main/index.html' )
 
+
+
+def service_worker(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    path=os.path.join(BASE_DIR,'templates','serviceWorker.js')
+    response=HttpResponse(open(path).read(),content_type='application/javascript')
+    return response
+
+def manifest(request):
+    return render(request, 'manifest.json', content_type='application/json')
+    
+def offline(request):
+    return render(request,'offline.html' )   
 
 
 
